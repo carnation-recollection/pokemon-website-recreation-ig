15a5c5091d5b069f29be91c660c57d96947b8995
carnation-recollection <testmailforiraslova@yahoo.com>
2025-04-27 18:16:31 +0300

    правка в слайдере

--- slider.js
+++ slider.js
@@ -1,29 +1,280 @@
-const половинаЭкрана = document.querySelector(".mainbox").clientWidth * 0.5
-const карточка = document.querySelector(".pokemoncard").clientWidth + window.getComputedStyle(document.querySelector(".pokesliderwrap"), null).getPropertyValue("gap").slice(0, -2)/2
+const container = document.querySelector(".mainbox");
+const cards = document.querySelectorAll(".pokemoncard");
+const sliderWrap = document.querySelector(".pokesliderwrap");
+const gap = parseInt(getComputedStyle(sliderWrap).gap) || 0;
 
-// номер активной карточки 
-let currentСard = 1
+const inactiveWidth = 250;  // Ширина неактивной карточки
+const activeWidth = 360;   // Ширина активной карточки
+const screenWidth = container.clientWidth;
 
-if (половинаЭкрана * 2 > 500) {
+// Проверяем, поместится ли вторая карточка (активная) при активации
+const secondCardFits = (inactiveWidth + gap + activeWidth) <= screenWidth;
 
-    let точка = 0
-    let шаг = 0
+let currentCard = -1
 
-    for (i = 0; i < 7; i++) {
-        точка = точка + карточка
-        if (половинаЭкрана - точка >= 0) {
-            шаг = i
-        }
-        else {
-            break;
-        }
 
+if (!secondCardFits) {
+    // Если не помещается - активируем первую карточку
+    cards[0].classList.add("shown");
+    currentCard = 1;
+} else {
+    // Иначе активируем центральную карточку
+    const cardsInHalfScreen = Math.floor(screenWidth / 2 / (inactiveWidth + gap));
+    currentCard = Math.min(cardsInHalfScreen + 1, cards.length);
+    cards[currentCard - 1].classList.add("shown");
+}
+
+console.log("Активная карточка:", currentCard);
+
+
+
+// проматывает слайдер влево
+function sliderScrollLeft() {
+
+    if (currentCard == 1) {
+        //sliderScrollLeft() doesn't work 
+    }
+    else {
+        //sliderScrollLeft works
+        currentCard = currentCard - 1
+
+        setCardActive(currentCard)
+        var slider = document.querySelector(".pokesliderwrap")
+        console.log(slider)
+
+        sliderPosition = sliderPosition + 253
+        let scrolledPx = sliderPosition
+        slider.style.transform = `translatex(${scrolledPx}px)`
+    }
+}
+
+// проматывает слайдер вправо
+function sliderScrollRight() {
+
+    if (currentCard == 7) {
+    }
+    else {
+        currentCard = currentCard + 1
+        var slider = document.querySelector(".pokesliderwrap")
+        console.log(slider)
+        setCardActive(currentCard)
+        sliderPosition = sliderPosition - 253
+        let scrolledPx = sliderPosition
+        slider.style.transform = `translatex(${scrolledPx}px)`
     }
 
-    console.log("номер активной карты = ", шаг + 2)
-    document.querySelectorAll(".pokemoncard")[шаг + 1].classList.add("shown")
-    currentСard= шаг + 2
+}
+
+//clearShown убирает класс shown с карточек
+function clearShown() {
+    var cards = document.querySelectorAll(".pokemoncard")
+    console.log(cards)
+    // цикл перебирает карточки
+    cards.forEach((card) => {
+        card.classList.remove("shown")
+    })
+}
+
+//setCardActive(0) // долив
+function setCardActive(cardnumber) {
+    clearShown()
+    //let cardnumber = 3
+    var cards = document.querySelectorAll(".pokemoncard")
+    console.log(cards[cardnumber - 1])
+
+    let конкретнаяКарточка = cards[cardnumber - 1]
+    конкретнаяКарточка.classList.add("shown")
+}
+
+
+
+/* Механика движения слайдера с помощью мышки НАЧАЛО */
+/* Механика движения слайдера с помощью мышки НАЧАЛО */
+
+
+// позиция слайдера
+let sliderPosition = 0
+
+// точка в которой кликнули мышкой на слайдер
+let clickStartPoint = 0
+
+// mouseactive - состояние мыши, зажата/не зажата
+// mouseactive = false - мышь не зажата (начальное состояние)
+let mouseActive = false
+
+// наш слайдер
+const slider = document.querySelector(".pokesliderwrap")
+
+// какая была позиция слайдера когда я ткнул мышкой
+let savedPosition = 0
+
+function swipeStart(событие) {
+    // запоминаем где начали тянуть слайдер (его сдвиг)
+    savedPosition = sliderPosition
+    // убираем transition когда начали дергать слайдер
+    slider.classList.add("noTransition")
+    // получить координаты где мы зажали мышку
+    clickStartPoint = событие.clientX
+    console.log("start point = ", clickStartPoint);
+    // состояние мыши - зажата
+    mouseActive = true
+}
+
+// фывфыв
+
+let растояниеКонца = 0
+let растояниеСтарта = 0
+
+function swipeAction(событие) {
+    // тут мы вычисляем куда двигать слайдер, в зависимости от всего другого
+    let currentPoint = событие.clientX
+
+    // обработка движений мышки
+    if (mouseActive == true) {
+
+        let кудаМотаем = "хз"
+        if (currentPoint - clickStartPoint > 0) {
+            кудаМотаем = "в лево"
+        }
+        else {
+            кудаМотаем = "в право"
+        }
+
+        console.log(кудаМотаем)
+
+
+
+        if (sliderPosition >= 430 && кудаМотаем == "в лево") {
+
+            let shift = (currentPoint - clickStartPoint) + savedPosition
+            console.log("Сдвиг = ", currentPoint - clickStartPoint)
+            console.log("Текущая точка = ", currentPoint)
+            //if()
+            let test = 430 + (currentPoint - clickStartPoint) / 4
+            console.log("Ставлю на позицию", test)
+            moveSliderTo(test)
+
+            // РАЗОБРАТЬСЯ ЧТО ТУТ ДЕЛИТЬ НА 10 ЧТОБ МЕДЛЕННО ДВИГАЛОСЬ 
+
+            // let shift = (currentPoint - clickStartPoint) + savedPosition
+            // console.log("Сдвиг = ", currentPoint - clickStartPoint)
+            // // двигаем слайдер
+            // moveSliderTo(shift)
+
+            // let changePoint = карточка * (currentCard - 3)
+
+            // // если мы мотали и дошли до точек -250, -500, -750 ..... и тд
+            // // то переключаем карточку на следующую
+            // if (-карточка * (currentCard - 2) < sliderPosition) {
+            // }
+            // else {
+            //     currentCard = currentCard + 1
+            //     setCardActive(currentCard)
+            // }
+
+            // // если мы мотали и дошли до точек -250, -500, -750 ..... и тд
+            // // то переключаем карточку на предыдущую
+            // if (-карточка * (currentCard - 3) < sliderPosition) {
+            //     currentCard = currentCard - 1
+            //     setCardActive(currentCard)
+            // }
+            // else {
+            // }
+
+
+
+        }
+
+        else {
+
+            //растояниеСтарта
+
+            let shift = currentPoint - clickStartPoint + savedPosition
+            //console.log("Сдвиг (норма) = ", currentPoint - clickStartPoint)
+            console.log("Слайдер-позиция = ", shift)
+            // двигаем слайдер
+            moveSliderTo(shift)
+
+            let changePoint = карточка * (currentCard - 3)
+
+            // если мы мотали и дошли до точек -250, -500, -750 ..... и тд
+            // то переключаем карточку на следующую
+            if (-карточка * (currentCard - 2) < sliderPosition) {
+            }
+            else {
+                currentCard = currentCard + 1
+                setCardActive(currentCard)
+            }
+
+            // если мы мотали и дошли до точек -250, -500, -750 ..... и тд
+            // то переключаем карточку на предыдущую
+            if (-карточка * (currentCard - 3) < sliderPosition) {
+                currentCard = currentCard - 1
+                setCardActive(currentCard)
+            }
+            else {
+            }
+
+        }
+
+
+
+
+    }
+    else {
+
+    }
+}
+
+function swipeEnd() {
+    if (sliderPosition > 430) {
+        moveSliderTo(430)
+    }
+    // при отпускании мыши
+    console.log("Конец")
+    // добавляем обратно transition 
+    slider.classList.remove("noTransition")
+    // состояние мыши - не зажата
+    mouseActive = false
+}
+
+function moveSliderTo(pointX) {
+    // передвигаем слайдер
+    slider.style.transform = `translatex(${pointX}px)`
+    // запомнили позицию слайдера
+    sliderPosition = pointX
+}
+
+
+
+/* Механика движения слайдера с помощью мышки КОНЕЦ */
+/* Механика движения слайдера с помощью мышки КОНЕЦ */
+let sliderBlock = document.querySelector(".pokesliderwrap")
+
+//slider ne pokazan na stranice 
+if (sliderBlock == null) {
+    // nichego
 }
 else {
-    document.querySelectorAll(".pokemoncard")[0].classList.add("shown")
+
+    // прекращаем прокрутку слайдера когда мышка покидает страницу
+    addEvent(document, "mouseout", function (e) {
+        e = e ? e : window.event;
+        var from = e.relatedTarget || e.toElement;
+        if (!from || from.nodeName == "HTML") {
+            // stop your drag event here
+            // for now we can just use an alert
+            swipeEnd()
+        }
+    });
+
+}
+
+function addEvent(obj, evt, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(evt, fn, false);
+    }
+    else if (obj.attachEvent) {
+        obj.attachEvent("on" + evt, fn);
+    }
 }
